e7aef3213f0b30c49a08a71e9be4a226bad30fe7
TannerHollis <40923236+TannerHollis@users.noreply.github.com>
2021-08-31 10:13:16 -0400

    Updated Resistor Combination
    Updated Resistor Combination

--- Water_Valve_Calculations/Water_Valve_Calculations.py
+++ Water_Valve_Calculations/Water_Valve_Calculations.py
@@ -5,18 +5,18 @@ r_water = 1.8e5
 def combo_str(combo):
     string = ""
     for j in range(len(combo)):
-        string = string + "{}".format(combo[j]) + ","
+        string = string + "{}".format(combo[j]) + "\t"
     return string
 
 resistances = [r_water * d * 1e-3 for d in distances]
-combos = "R1, R2, R3, R4, R5"
+combos = "R1\tR2\tR3\tR4\tR5"
 r_combos = []
 r_totals = []
 
 for i in range(len(resistances)):
     for j in range(i, len(resistances)):
         r_temp = 0
-        r_combo = [0,0,0,0,0]
+        r_combo = [0] * len(distances)
         for k in range(i, j + 1):
             r_combo[k] = 1
             r_temp = r_temp + 1 / resistances[k]
@@ -27,6 +27,6 @@ percentages = [r / (r_top + r) for r in r_totals]
 
 with open('total_resistor_combinations.txt', 'w') as f:
     f.write("Different Combinations: {}\n".format(len(percentages)))
-    f.write("{}, Vout/Vin, Resistance(Ohms)\n".format(combos))
+    f.write("{}\tVout/Vin\tResistance(Ohms)\n".format(combos))
     for i in range(len(percentages)):
-        f.write("{}{},{}\n".format(combo_str(r_combos[i]), percentages[i] * 100, r_totals[i]))
+        f.write("{}{:.5f}\t\t{:.5f}\n".format(combo_str(r_combos[i]), percentages[i] * 100, r_totals[i]))
